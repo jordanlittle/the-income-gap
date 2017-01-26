3e106ee940b8685f999a15962fefbf4b5f5a5849
Jordan Little <iamjordanlittle@gmail.com>
2017-01-25 21:59:49 -0600

    setting up proj
    adding files

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		sass: {
+			options: {
+				sourceMap: false,
+				outputStyle: 'compressed'
+			},
+			dist: {
+				files: {
+				    'www/style.css': 'build/build.scss'
+				}
+			}
+		},
+
+		autoprefixer: {
+			dist: {
+				files: {
+					'www/style.css': 'www/style.css'
+				},
+				options: {
+					browsers: ['last 2 versions']
+				}
+			}
+		},
+
+		bake: {
+	        dist: {
+	            options: {
+	                content: "config.json"
+	            },	 
+	            files: {
+	                "www/index.html" : "build/homepage.html",
+	            }
+	        }
+	    },
+
+		watch: {
+	        styles: {
+                files: ['scss/*.scss', 'build/**/*.html'],
+                tasks: ['sass', 'autoprefixer', 'bake'],
+                options: {
+                    spawn: false
+                }
+            }
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.registerTask('default', ['watch']);
+};
